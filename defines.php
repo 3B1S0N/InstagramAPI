fde9ee3608407b3f503ae8f59589a8f9123bcf63
Tobias Woode <56300965+3B1S0N@users.noreply.github.com>
2022-03-08 01:10:11 +0000

    Delete defines.php

--- defines.php
+++
@@ -1,7 +0,0 @@
-<?php
-    define("FACEBOOK_APP_ID","992086681745720");
-    define("FACEBOOK_APP_SECRET","1a5a29188acaa75587baa42857428e08");
-    define("FACEBOOK_REDIRECT_URI","https://instagram-api-yg3s5.ondigitalocean.app/");
-
-    
-?>

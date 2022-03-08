260345196de1ad3b3a4777b4ffef82915ca411fe
Tobias Woode <56300965+3B1S0N@users.noreply.github.com>
2022-03-08 17:46:57 +0000

    Add files via upload

---
+++ defines.php
@@ -0,0 +1,5 @@
+<?PHP
+define('INSTAGRAM_APP_ID', '1021060078488127');
+define('INSTAGRAM_APP_SECRET', '39445e69b5822f5ca5ec34f2a5755167');
+define('INSTAGRAM_APP_REDIRECT_URI','https://instagram-api-yg3s5.ondigitalocean.app/instagram_basic_api.php/');
+?>

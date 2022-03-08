4f891f32ea5889249aa02e6ea47aa84a8da1cd4b
Tobias Woode <56300965+3B1S0N@users.noreply.github.com>
2022-03-08 18:09:11 +0000

    Update index.php

--- index.php
+++ index.php
@@ -14,7 +14,8 @@ $ig = new instagram_basic_api($params);
 <h1>Instagram Basic Display API</h2>
 </hr>
 <?php if ($ig-> hasUserAccessToken) : ?>
-    echo 'yes';
+    <h4>IG Info</h4>
+    <?php echo $ig-> getUserAccessToken();?>
 
 <?php else : ?>
     <a href = "<?php echo $ig->authorizationUrl; ?>">

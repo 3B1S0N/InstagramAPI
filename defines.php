e70e566f2cae00a3aea5d1f90a9481393526d922
Tobias Woode <56300965+3B1S0N@users.noreply.github.com>
2022-03-08 01:49:48 +0000

    Update defines.php

--- defines.php
+++ defines.php
@@ -1,8 +1,8 @@
 <?php
 
     session_start();
-    define("FACEBOOK_APP_ID","992086681745720");
-    define("FACEBOOK_APP_SECRET","1a5a29188acaa75587baa42857428e08");
+    define("FACEBOOK_APP_ID","486850539594724");
+    define("FACEBOOK_APP_SECRET","08f44fb309baf3c5920c454b958517b3");
     define("FACEBOOK_REDIRECT_URI","https://instagram-api-yg3s5.ondigitalocean.app/");
 
 
